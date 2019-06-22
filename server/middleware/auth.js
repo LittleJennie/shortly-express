a36d82fbdcfebaf6ea0df69078a30240c9cf3696
littlejennie <jenniezeng1@gmail.com>
2019-06-21 20:22:23 -0700

    End of Day 1

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,29 @@
 const models = require('../models');
 const Promise = require('bluebird');
 
+// In middleware/auth.js, write a createSession middleware function that accesses the parsed cookies on the request,
+// looks up the user data related to that session, and assigns an object to a session property on
+// the request that contains relevant user information. (Ask yourself: what information about the user
+// would you want to keep in this session object?)
+
+// An incoming request with no cookies should generate a session with a unique hash and store it the 
+// sessions database. The middleware function should use this unique hash to set a cookie in the response headers. 
+// (Ask yourself: How do I set cookies using Express?).
+
+// If an incoming request has a cookie, the middleware should verify that the cookie is valid 
+// (i.e., it is a session that is stored in your database).
+// If an incoming cookie is not valid, what do you think you should do with that session and cookie?
+
 module.exports.createSession = (req, res, next) => {
+  console.log('cookies: ', req.cookies);
+  if ((Object.keys(req.cookies)).length === 0) {
+    return models.Sessions.create()
+      .then(() => {
+        models.Sessions.get();
+      });
+  } else {
+      
+  }
 };
 
 /************************************************************/
